170f13d054b43693d6f3a031361e1ddd72723adc
erikag213 <erika.gomez@chicagobooth.edu>
2021-05-19 21:22:32 +0000

    76%

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action(:load_current_user)
   
   # Uncomment this if you want to force users to sign in before any other actions
-  # before_action(:force_user_sign_in)
+  #before_action(:force_user_sign_in)
   
   def load_current_user
     the_id = session[:user_id]
@@ -16,7 +16,6 @@ class ApplicationController < ActionController::Base
   end
 
   def home
-  
 
     render({ :template => "home/homepage.html.erb" })
   end
